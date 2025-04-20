ad243892280319537a7056346c7db15f3f558d5d
almogbaryossef <abaryossef@ucsd.edu>
2025-04-20 16:36:39 -0700

    comitting all recent changes, created files mostly

---
+++ physics-engine/PhysicsWorld.h
@@ -0,0 +1,9 @@
+
+#include <vector>
+# include <memory>
+
+class RigidBodies;
+class Collider;
+
+RigidBodies* rigidBodies;
+
---
+++ physics-engine/RigidBody.h
@@ -0,0 +1,5 @@
+struct RigidBody {
+    vec3 position;
+    vec3 velocity;
+    vec3 force;
+}
